7dce9399b2c2a4ad40ce92c594cb32517479966f
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-08-24 00:17:09 -0600

    Removal of unneeded files

--- IoTivity-lite/example/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CURPWD=`pwd`
-PROJNAME=${PWD##*/}
-
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/Makefile $CURPWD/src/"
-eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/example/example.json $CURPWD/$PROJNAME.json"
-eval ${MY_COMMAND}
