fbe7861daccbdb7303c48d054cbe24386e66990e
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-25 13:48:00 -0600

    script updates to account for architecture

--- IoTivity/build2.sh
+++ IoTivity/build2.sh
@@ -6,15 +6,17 @@ MY_COMMAND="cd ${OCFPATH}/iotivity"
 eval ${MY_COMMAND}
 
 #TODO change this to compile from the project source direcotry, but temporarily copy the souce code over.
-MY_COMMAND="cp $CURPWD/src/*.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
+MY_COMMAND="cp -f $CURPWD/src/*.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
 eval ${MY_COMMAND}
-MY_COMMAND="cp $CURPWD/src/*.h ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
+MY_COMMAND="cp -f $CURPWD/src/*.h ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
 eval ${MY_COMMAND}
 MY_COMMAND="mv -f ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/$PROJNAME.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/server.cpp"
 eval ${MY_COMMAND}
+MY_COMMAND="touch ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/server.cpp"
+eval ${MY_COMMAND}
 
 # copying the SConscript file to the source folder
-MY_COMMAND="cp $CURPWD/SConscript ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
+MY_COMMAND="cp -f $CURPWD/SConscript ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
 eval ${MY_COMMAND}
 
 #scons resource/examples
@@ -22,7 +24,7 @@ MY_COMMAND="scons examples/OCFDeviceBuilder"
 eval ${MY_COMMAND}
 
 #TODO remove this command once the above problem is fixed
-MY_COMMAND="cp ${OCFPATH}/iotivity/out/linux/armv7l/release/examples/OCFDeviceBuilder/server /$CURPWD/bin/$PROJNAME"
+MY_COMMAND="cp -f ${OCFPATH}/iotivity/out/linux/armv7l/release/examples/OCFDeviceBuilder/server /$CURPWD/bin/$PROJNAME"
 eval ${MY_COMMAND}
 
 cd $CURPWD
--- IoTivity/gen2.sh
+++ IoTivity/gen2.sh
@@ -4,24 +4,24 @@ PROJNAME=${PWD##*/}
 
 MY_COMMAND="cd ${OCFPATH}/DeviceBuilder"
 eval ${MY_COMMAND}
-MY_COMMAND="sh ./DeviceBuilder_C++IotivityServer.sh $CURPWD/$PROJNAME.json  $CURPWD/device_output \"oic.d.light\""
+MY_COMMAND="sh ./DeviceBuilder_C++IotivityServer.sh $CURPWD/$PROJNAME.json  $CURPWD/device_output \"oic.d.widget\""
 eval ${MY_COMMAND}
 
 # copying the introspection file to the executable folder
-MY_COMMAND="cp $CURPWD/device_output/code/server_introspection.dat $CURPWD/bin/"
+MY_COMMAND="cp -f $CURPWD/device_output/code/server_introspection.dat $CURPWD/bin/"
 eval ${MY_COMMAND}
 
 # quick fix: using the iotivity supplied oic_svr_db_server_justworks.dat file
-MY_COMMAND="cp ${OCFPATH}/iotivity/resource/csdk/security/provisioning/sample/oic_svr_db_server_justworks.dat $CURPWD/bin/server_security.dat"
+MY_COMMAND="cp -f ${OCFPATH}/iotivity/resource/csdk/security/provisioning/sample/oic_svr_db_server_justworks.dat $CURPWD/bin/server_security.dat"
 eval ${MY_COMMAND}
 
 if [ -e $CURPWD/src/$PROJNAME.cpp ]
 then
   echo "It appears that you have modified the automatically generated source file. Use a tool like diff3 if you want to merge in any changes."
 else
-  MY_COMMAND="cp $CURPWD/device_output/code/server.cpp $CURPWD/src/$PROJNAME.cpp"
+  MY_COMMAND="cp -i $CURPWD/device_output/code/server.cpp $CURPWD/src/$PROJNAME.cpp"
   eval ${MY_COMMAND}
-  MY_COMMAND="cp $CURPWD/device_output/code/server.cpp $CURPWD/src/$PROJNAME.cpp.gen"
+  MY_COMMAND="cp -i $CURPWD/device_output/code/server.cpp $CURPWD/src/$PROJNAME.cpp.gen"
   eval ${MY_COMMAND}
 fi
 
