6e9e0aa2274604b01f3c2b03a0bf4d27c0517787
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-25 14:01:44 -0600

    modified build2

--- IoTivity/build2.sh
+++ IoTivity/build2.sh
@@ -2,29 +2,21 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-MY_COMMAND="cd ${OCFPATH}/iotivity"
-eval ${MY_COMMAND}
+cd ${OCFPATH}/iotivity
 
 #TODO change this to compile from the project source direcotry, but temporarily copy the souce code over.
-MY_COMMAND="cp -f $CURPWD/src/*.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
-eval ${MY_COMMAND}
-MY_COMMAND="cp -f $CURPWD/src/*.h ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
-eval ${MY_COMMAND}
-MY_COMMAND="mv -f ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/$PROJNAME.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/server.cpp"
-eval ${MY_COMMAND}
-MY_COMMAND="touch ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/server.cpp"
-eval ${MY_COMMAND}
+cp -f ${CURPWD}/src/*.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/
+cp -f ${CURPWD}/src/*.h ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/
+mv -f ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/${PROJNAME}.cpp ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/server.cpp
+touch ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/server.cpp
 
 # copying the SConscript file to the source folder
-MY_COMMAND="cp -f $CURPWD/SConscript ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/"
-eval ${MY_COMMAND}
+cp -f ${CURPWD}/SConscript ${OCFPATH}/iotivity/examples/OCFDeviceBuilder/
 
 #scons resource/examples
-MY_COMMAND="scons examples/OCFDeviceBuilder"
-eval ${MY_COMMAND}
+scons examples/OCFDeviceBuilder
 
 #TODO remove this command once the above problem is fixed
-MY_COMMAND="cp -f ${OCFPATH}/iotivity/out/linux/armv7l/release/examples/OCFDeviceBuilder/server /$CURPWD/bin/$PROJNAME"
-eval ${MY_COMMAND}
+cp -f ${OCFPATH}/iotivity/out/linux/x86_64/release/examples/OCFDeviceBuilder/server /$CURPWD/bin/${PROJNAME}
 
 cd $CURPWD
