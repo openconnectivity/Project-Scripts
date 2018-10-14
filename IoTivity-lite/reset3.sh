6c581ef5ea5825511e7f21be59e0216bea526d9d
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-13 18:46:33 -0600

    fixing reset3

--- IoTivity-lite/reset3.sh
+++ IoTivity-lite/reset3.sh
@@ -2,7 +2,7 @@
 OCFBASEPATH=`jq --raw-output '.ocf_base_path' ${CURPWD}/${PROJNAME}-config.json`
 
 # TODO Go through DeviceBuilder for each of the implementations and platforms (just doing the first array element for this example)
-OCFSUBPATH=`jq --raw-output '.implementation_paths[0]' ${CURPWD}/${PROJNAME}-config.json`
+OCFSUBPATH=`jq --raw-output '.implementation_paths[0]' ${CURPWD}/${PROJNAME}-config.jsonbash -x `
 OCFPATH="${OCFBASEPATH}${OCFSUBPATH}"
 
 if [ "$OCFSUBPATH" == "/iot" ]; then
