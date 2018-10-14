3c2aa9b76dae6aa59740cdbb939ccf615dc269c3
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-13 20:39:48 -0600

    create project

--- create_project.sh
+++ create_project.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 PROJNAME=$1
-
-OCFBASEPATH=`jq --raw-output '.ocf_base_path' ${CURPWD}/${PROJNAME}-config.json`
-
-# TODO Go through DeviceBuilder for each of the implementations and platforms (just doing the first array element for this example)
-OCFSUBPATH=`jq --raw-output '.implementation_paths[0]' ${CURPWD}/${PROJNAME}-config.json`
-OCFPATH="${OCFBASEPATH}${OCFSUBPATH}"
-PLATFORM=`jq --raw-output '.platforms[0]' ${CURPWD}/${PROJNAME}-config.json`
+CURPWD=`pwd`
 
 mkdir -p ./${PROJNAME}
 mkdir -p ./${PROJNAME}/src
 mkdir -p ./${PROJNAME}/bin
-cp ${OCFPATH}/default.json ./$PROJNAME/$PROJNAME.json
 
 cd ${PROJNAME}
+echo "{" > ${PROJNAME}-config.json
+echo "  \"friendly_name\" : \"Switch\"," >> ${PROJNAME}-config.json
+echo "  \"device_type\" : \"oic.d.switchdevice\"," >> ${PROJNAME}-config.json
+echo "  \"ocf_base_path\" : \"~\"," >> ${PROJNAME}-config.json
+echo "  \"implementation_paths\" : [" >> ${PROJNAME}-config.json
+echo "    \"/iot\"," >> ${PROJNAME}-config.json
+echo "    \"/iot-lite\"," >> ${PROJNAME}-config.json
+echo "    \"~/new-imp\"" >> ${PROJNAME}-config.json
+echo "  ]," >> ${PROJNAME}-config.json
+echo "  \"platforms\" : [" >> ${PROJNAME}-config.json
+echo "    \"linux\"," >> ${PROJNAME}-config.json
+echo "    \"windows\"," >> ${PROJNAME}-config.json
+echo "    \"android\"" >> ${PROJNAME}-config.json
+echo "  ]," >> ${PROJNAME}-config.json
+echo "  \"device_description\" : [" >> ${PROJNAME}-config.json
+echo "    {" >> ${PROJNAME}-config.json
+echo "      \"path\" : \"/binaryswitch\"," >> ${PROJNAME}-config.json
+echo "      \"rt\"   : [ \"oic.r.switch.binary\" ]," >> ${PROJNAME}-config.json
+echo "      \"if\"   : [\"oic.if.a\", \"oic.if.baseline\" ]," >> ${PROJNAME}-config.json
+echo "      \"remove_properties\" : [ \"range\", \"step\" , \"id\", \"precision\" ]" >> ${PROJNAME}-config.json
+echo "    }," >> ${PROJNAME}-config.json
+echo "    {" >> ${PROJNAME}-config.json
+echo "      \"path\" : \"/oic/p\"," >> ${PROJNAME}-config.json
+echo "      \"rt\"   : [ \"oic.wk.p\" ]," >> ${PROJNAME}-config.json
+echo "      \"if\"   : [\"oic.if.baseline\", \"oic.if.r\" ]," >> ${PROJNAME}-config.json
+echo "      \"remove_properties\" : [ \"n\", \"range\", \"value\", \"step\", \"precision\", \"vid\"  ]" >> ${PROJNAME}-config.json
+echo "    }" >> ${PROJNAME}-config.json
+echo "  ]" >> ${PROJNAME}-config.json
+echo "}" >> ${PROJNAME}-config.json
+
+cd ${CURPWD}
