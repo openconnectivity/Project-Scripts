d1a5191adb4890071e66f698ca780deae1be3c24
Clarke Stevens <clarke.stevens@sjrb.ca>
2020-10-21 12:25:06 -0600

    New scripts for esp32

--- reset_esp32.sh
+++ reset_esp32.sh
@@ -10,28 +10,9 @@ OCFSUBPATH=`jq --raw-output '.implementation_paths[0]' ${CURPWD}/${PROJNAME}-con
 OCFPATH="${OCFBASEPATH}${OCFSUBPATH}"
 PLATFORM=`jq --raw-output '.platforms[0]' ${CURPWD}/${PROJNAME}-config.json`
 
-if [  "$OCFSUBPATH" == "/iot-lite" ]; then
-  #TODO change this to compile from the project source direcotry, but temporarily copy the souce code over.
-  MY_COMMAND="cp ${CURPWD}/src/${PROJNAME}.c ${OCFPATH}/../iotivity-lite/port/main/esp32-example.c"
-  eval ${MY_COMMAND}
-  MY_COMMAND="cp ${CURPWD}/src/${PROJNAME}-main.c ${OCFPATH}/../iotivity-lite/port/main/main.c"
-  eval ${MY_COMMAND}
-  MY_COMMAND="cp ${CURPWD}/src/pki_certs.h ${OCFPATH}/../iotivity-lite/port/main/pki_certs.h"
-  eval ${MY_COMMAND}
-
-  MY_COMMAND="cd ${OCFPATH}/../iotivity-lite/port/${PLATFORM}/"
-  eval ${MY_COMMAND}
-  MY_COMMAND="idf.py set-target esp32"
-  eval ${MY_COMMAND}
-  MY_COMMAND="idf.py erase_flash"
-  eval ${MY_COMMAND}
-  MY_COMMAND="idf.py build"
-  eval ${MY_COMMAND}
-  MY_COMMAND="idf.py -p /dev/ttyUSB0 flash monitor"
-  eval ${MY_COMMAND}
-  #The executable will be uploaded and run on the Arduino as soon as it is compiled
-else
-  No OCFSUBPATH: $OCFSUBPATH
-fi
+MY_COMMAND="cd ${OCFPATH}/iotivity-lite/port/${PLATFORM}/"
+eval ${MY_COMMAND}
+MY_COMMAND="idf.py erase_flash"
+eval ${MY_COMMAND}
 
 cd $CURPWD
