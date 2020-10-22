99f9ff2f3fa30e854ddbc67b22df4aa55d96b0e7
Clarke Stevens <clarke.stevens@sjrb.ca>
2020-10-21 21:02:39 -0600

    Updated scripts for esp32

--- reset.sh
+++ reset.sh
@@ -6,8 +6,17 @@ OCFBASEPATH=`jq --raw-output '.ocf_base_path' ${CURPWD}/${PROJNAME}-config.json`
 # TODO Go through DeviceBuilder for each of the implementations and platforms (just doing the first array element for this example)
 OCFSUBPATH=`jq --raw-output '.implementation_paths[0]' ${CURPWD}/${PROJNAME}-config.json`
 OCFPATH="${OCFBASEPATH}${OCFSUBPATH}"
+PLATFORM=`jq --raw-output '.platforms[0]' ${CURPWD}/${PROJNAME}-config.json`
 
-if [ "$OCFSUBPATH" == "/iot" ]; then
+if [ "$PLATFORM" == "esp32" ]; then
+  MY_COMMAND="cd ${OCFPATH}/iotivity-lite/port/${PLATFORM}/"
+  eval ${MY_COMMAND}
+  MY_COMMAND="idf.py erase_flash"
+  eval ${MY_COMMAND}
+  cd $CURPWD
+elif [ "$PLATFORM" == "arduino" ]; then
+  echo "Arduino reset"
+elif [ "$OCFSUBPATH" == "/iot" ]; then
   rm -f ./bin/server_security.dat
   MY_COMMAND="cp ${OCFPATH}/iotivity/resource/csdk/security/provisioning/sample/oic_svr_db_server_justworks.dat ./bin/server_security.dat"
   eval ${MY_COMMAND}
--- run.sh
+++ run.sh
@@ -2,7 +2,16 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-cd ./bin
+PLATFORM=`jq --raw-output '.platforms[0]' ${CURPWD}/${PROJNAME}-config.json`
 
-./${PROJNAME}
+if [ "$PLATFORM" == "esp32" ]; then
+  MY_COMMAND="cd ${OCFPATH}/iotivity-lite/port/${PLATFORM}/"
+  eval ${MY_COMMAND}
+  MY_COMMAND="idf.py -p /dev/ttyUSB0 flash monitor"
+  eval ${MY_COMMAND}
+else
+  cd ./bin
+  ./${PROJNAME}
+fi
+
 cd ${CURPWD}
